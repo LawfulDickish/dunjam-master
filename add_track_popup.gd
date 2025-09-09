28810c1f5d2cf2369fee349f5b9a64433fd5ae97
LawfulDickish <36271525+LawfulDickish@users.noreply.github.com>
2025-09-09 11:59:28 -0400

    Add_Track_Popup Functionality Test Push
    Set the player to be its own scene and refactored the code so that it could be instantiated in the main window and pull double duty as the track player. Changed the song progress tracker to a scroll bar instead of a value bar, and shifted the value bar to control volume. Added the ability to numerically set the timestamps for tags, to allow for more precise adjustments. Will need to do some user testing to make sure interface is functional before moving on to core functionality

--- add_track_popup.gd
+++ add_track_popup.gd
@@ -10,60 +10,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $SongProgress/AudioStreamPlayer.playing:
-		$SongProgress.value = $SongProgress/AudioStreamPlayer.get_playback_position()
 	pass
-
 
 func _on_file_select_button_pressed() -> void:
 	$TrackSelect.visible = true
 	pass # Replace with function body.
 
 func _on_track_select_file_selected(path: String) -> void:
-	$TagTrack.addtrack(path)
-	$Tags/List.clear()
-	$SongProgress/AudioStreamPlayer.stream = $TagTrack.track
-	$SongProgress.max_value = $SongProgress/AudioStreamPlayer.stream.get_length()
-	$SongProgress.value = 0
-	$SongProgress/Timecode.text = timecode_text($SongProgress.value) + " / " + timecode_text($SongProgress.max_value)
-	$SongProgress/PlayPause.button_pressed = false
-	
+	$PlayerWidget.load_track_by_file(path)
 	$FilePath.text = path
 	$NameInput.text = path.substr(path.rfind("/")+1,path.rfind(".")-path.rfind("/")-1) 
 	#Auto-completes the name based on the filename by grabbing a substring between the last instance of the path "/" and the filename "."
 	pass # Replace with function body.
 
 
-func _on_play_pause_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		$SongProgress/AudioStreamPlayer.play($SongProgress.value)
-	else:
-		$SongProgress/AudioStreamPlayer.stop()
-	pass # Replace with function body.
-
-
-func _on_song_progress_value_changed(value: float) -> void:
-	$SongProgress/Timecode.text = timecode_text($SongProgress.value) + " / " + timecode_text($SongProgress.max_value)
-	if $SongProgress/AudioStreamPlayer.playing and dragged:
-		$SongProgress/AudioStreamPlayer.seek($SongProgress.value)
-	pass # Replace with function body.
-
-func timecode_text(length: float, precision: float = 2) -> String:
-	var format = "%d:%0{0}.{1}f"
-	var offset = 3 if precision > 0 else 2
-	format = format.format([precision + offset, precision])
-	var timecode = format % [ length / 60 , length - (floor(length / 60) * 60)]
-	return timecode
-
-func text_timecode(timecode: String):
-	var length
-	var minutes = timecode.split(":")[0] as int
-	var seconds = timecode.split(":")[1] as float
-	length = minutes * 60.0 + seconds
-	return length
-
 func get_tag_name(tag: String) -> String: return tag.split(SEPARATOR)[1]
-func get_tag_timecode(tag: String) -> float: return text_timecode(tag.split(SEPARATOR)[0])
+func get_tag_timecode(tag: String) -> float: return $PlayerWidget.text_timecode(tag.split(SEPARATOR)[0])
 
 func get_tag_list() -> Array:
 	var names = []
@@ -71,42 +33,48 @@ func get_tag_list() -> Array:
 		names.append($Tags/List.get_item_text(x).split(SEPARATOR)[1])
 	return names
 
-
-func _on_song_progress_drag_started() -> void:
-	dragged = true
-	pass # Replace with function body.
-
-
-func _on_song_progress_drag_ended(value_changed: bool) -> void:
-	dragged = false
-	pass # Replace with function body.
-
-
-func _on_audio_stream_player_finished() -> void:
-	$SongProgress/PlayPause.button_pressed = false
-	pass # Replace with function body.
-
-
 func _on_add_pressed() -> void:
 	if !($Tags/Input.text == ""):
-		var name_code = timecode_text($SongProgress.value) + SEPARATOR + $Tags/Input.text
+		var name_code = $PlayerWidget.timecode_text($PlayerWidget/Progress.value) + SEPARATOR + $Tags/Input.text
 		if ($Tags/Input.text in get_tag_list()):
 			$Tags/List.set_item_text(get_tag_list().find($Tags/Input.text),name_code)
 		else:
 			$Tags/List.add_item(name_code)
 		$Tags/Input.text = ""
-	pass # Replace with function body.
+	
+	#Sets the timecode spinners to the current time even if nothing is added to tags
+	set_time_spinners($PlayerWidget/Progress.value)
+	pass 
 
 
 func _on_remove_pressed() -> void:
 	for select in $Tags/List.get_selected_items():
 		$Tags/List.remove_item(select)
-	pass # Replace with function body.
+	pass 
 
 
 func _on_list_item_activated(index: int) -> void:
-	if $SongProgress/AudioStreamPlayer.playing:
-		$SongProgress/AudioStreamPlayer.seek(get_tag_timecode($Tags/List.get_item_text(index)))
+	if $PlayerWidget/AudioStreamPlayer.playing:
+		$PlayerWidget/AudioStreamPlayer.seek(get_tag_timecode($Tags/List.get_item_text(index)))
 	else:
-		$SongProgress.value = get_tag_timecode($Tags/List.get_item_text(index))
-	pass # Replace with function body.
+		$PlayerWidget/Progress.value = get_tag_timecode($Tags/List.get_item_text(index))
+	pass 
+
+
+func set_time_spinners(timestamp: float) -> void:
+	$Tags/Minutes.value = floor(timestamp / 60)
+	$Tags/Seconds.value = (timestamp as int) % 60
+	$Tags/Milliseconds.value = (timestamp - floor(timestamp)) * 100
+	pass 
+
+
+func _on_add_at_time_pressed() -> void:
+	if !($Tags/Input.text == ""):
+		var test = "%d:%02d.%02d" % [$Tags/Minutes.value,$Tags/Seconds.value,$Tags/Milliseconds.value]
+		var name_code = test + SEPARATOR + $Tags/Input.text
+		if ($Tags/Input.text in get_tag_list()):
+			$Tags/List.set_item_text(get_tag_list().find($Tags/Input.text),name_code)
+		else:
+			$Tags/List.add_item(name_code)
+		$Tags/Input.text = ""
+	pass 
---
+++ player.gd
@@ -0,0 +1,73 @@
+extends Control
+var natProg = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if $TagTrack.track != null:
+		$AudioStreamPlayer.stream = $TagTrack.track
+		$Progress.max_value = $AudioStreamPlayer.stream.get_length()
+		$Progress.value = 0
+		$Progress.step = .01
+		$Progress.page = 5
+		$Progress/PlayPause.button_pressed = false
+	pass 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if $AudioStreamPlayer.playing:
+		natProg = true
+		$Progress.value = $AudioStreamPlayer.get_playback_position()
+		natProg = false
+	pass
+
+
+func load_track_by_file(path: String) -> void:
+	$TagTrack.addtrack(path)
+	_ready()
+	pass
+
+func load_track(track: AudioStream) -> void:
+	$TagTrack.track = track
+	_ready()
+	pass
+
+func timecode_text(length: float, precision: float = 2) -> String:
+	var format = "%d:%0{0}.{1}f"
+	var offset = 3 if precision > 0 else 2
+	format = format.format([precision + offset, precision])
+	var timecode = format % [ length / 60 , length - (floor(length / 60) * 60)]
+	return timecode
+
+func text_timecode(timecode: String) -> float:
+	var length
+	var minutes = timecode.split(":")[0] as int
+	var seconds = timecode.split(":")[1] as float
+	length = minutes * 60.0 + seconds
+	return length
+
+func get_timestamp() -> float:
+	return $Progress.value
+
+func _on_play_pause_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$AudioStreamPlayer.play($Progress.value)
+	else:
+		$AudioStreamPlayer.stop()
+	pass
+	
+func _on_audio_stream_player_finished() -> void:
+	$Progress/PlayPause.button_pressed = false
+	pass
+
+func _on_progress_value_changed(value: float) -> void:
+	#Set timecode
+	$Progress/Timecode.text = timecode_text($Progress.value) + " / " + timecode_text($Progress.max_value)
+	if $AudioStreamPlayer.playing and !natProg:
+		$AudioStreamPlayer.seek($Progress.value)
+	pass
+
+
+func _on_volume_value_changed(value: float) -> void:
+	$AudioStreamPlayer.volume_db = value
+	pass # Replace with function body.
